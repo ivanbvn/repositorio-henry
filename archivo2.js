f834253be38219b2edf27b916638fee55d6682b8
Ivaan14 <bilvinas.ivan@gmail.com>
2022-11-03 13:45:18 -0300

    archivo2.js modified

---
+++ archivo2.js
@@ -0,0 +1,29 @@
+[33mcommit b8cdcafd23c63f0af0164531cc545df91b94f654[m[33m ([m[1;36mHEAD -> [m[1;32mmain[m[33m, [m[1;31morigin/main[m[33m, [m[1;31morigin/HEAD[m[33m)[m
+Author: ivanbvn <96452713+Ivaan14@users.noreply.github.com>
+Date:   Thu Nov 3 02:20:48 2022 -0300
+
+    added new file, archivo2.js
+
+[33mcommit e9a48aa4db8cc63f444ea8c71f27b18327ab9541[m
+Author: ivanbvn <96452713+Ivaan14@users.noreply.github.com>
+Date:   Thu Nov 3 02:18:25 2022 -0300
+
+    Added fullName function
+
+[33mcommit af860c3b8b59dc0278a6db1f48f08537dad702f1[m
+Author: ivanbvn <96452713+Ivaan14@users.noreply.github.com>
+Date:   Thu Nov 3 02:01:21 2022 -0300
+
+    Added name and lastName variables on ejemplo-archivo.js
+
+[33mcommit 1e1ca9c252f80f2aaedef9b30cf5b6362dabb10c[m
+Author: ivanbvn <96452713+Ivaan14@users.noreply.github.com>
+Date:   Thu Nov 3 01:49:14 2022 -0300
+
+    added new file, ejemplo-archivo.js
+
+[33mcommit fe5fee3fde51d63e67b169c24ecf7d36210f7228[m
+Author: ivanbvn <96452713+Ivaan14@users.noreply.github.com>
+Date:   Thu Nov 3 01:44:56 2022 -0300
+
+    Initial commit
